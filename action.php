6d44c5891c3c620f1ae035c0f6b8a628fb0e9521
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-09-10 11:25:50 +0200

    change variable name on action.php

--- action.php
+++ action.php
@@ -13,17 +13,17 @@ $prio = $res["priority"];
 // var_dump($_GET);
 if(isset($_GET['action']) && $_GET['action'] === "done" && isset($_GET['idtask'])){
 
-    $query = $dbCo->prepare("UPDATE tasks
+    $query1 = $dbCo->prepare("UPDATE tasks
     SET done = 1, priority = 0
     WHERE id_tasks = :idtasks;");
-    $query->execute([
+    $query1->execute([
         "idtasks" => $_GET['idtask']
     ]);
 
-    $query1 = $dbCo->prepare("UPDATE tasks
+    $query2 = $dbCo->prepare("UPDATE tasks
     SET priority = priority-1
     WHERE priority > $prio AND done = 0;");
-    $query1->execute();
+    $query2->execute();
 }
 
 
@@ -44,14 +44,6 @@ if(isset($_GET['action']) && $_GET['action'] === "up" && isset($_GET['idtask']))
 
 
 if(isset($_GET['action']) && $_GET['action'] === "down" && isset($_GET['idtask'])){
-
-    // $query = $dbCo->prepare("SELECT priority FROM tasks WHERE id_tasks = :idtasks AND done = 0;");
-    // $query->execute([
-    //     "idtasks" => $_GET['idtask']
-    // ]);
-    // $res = $query->fetch();
-    // $prio = $res["priority"];
-    // var_dump($prio);
 
     $query1 = $dbCo->prepare("UPDATE tasks
     SET priority = priority+1
